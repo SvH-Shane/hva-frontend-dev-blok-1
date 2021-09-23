abe7234893e7150230fe54692009b963788c56f7
Glitch (hello-webpage) <39694808+SvH-Shane@users.noreply.github.com>
2021-09-23 14:07:45 +0200

    Change js

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,7 @@
 // JavaScript Document
 
 //Drop the menu down nieuws!
-// TEMP: solution
 
-var buttonNieuws = document.querySelector(".button-nieuws ");
-var buttonSport = document.querySelector(".button-sport");
-var nieuws = document.querySelector(".nieuws");
-var sport = document.querySelector(".sport");
-
-buttonNieuws.addEventListener("click", function() {
-  nieuws.classList.toggle("open-nieuws");
-});
-
-buttonSport.addEventListener("click", function() {
-  nieuws.classList.toggle("open");
-  sport.classList.toggle("open-sport");
-});
+function myFunction(menuDropdown) {
+  menuDropdown.parentNode.getElementsByClassName("dropdown")[0].classList.toggle("open");
+}
